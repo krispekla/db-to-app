3a46dec3216308cc4b3585fa215e7cbbe6a64d5d
Kris Peklaric <peklarick@gmail.com>
2020-02-22 23:05:47 +0100

    added new screen

---
+++ PPPKProjekt/i3XML.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="i3XML.aspx.cs" Inherits="PPPKProjekt.i3XML" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2 class="m-3">i3 XML</h2>
+    <div class="container bg-light mt-3 p-5">
+        <div class="row mb-5">
+            <div class="col-4">
+                <span>Travel order:</span>
+                <asp:DropDownList CssClass="mb-3" ID="ddlTravelOrders" runat="server" AutoPostBack="True"
+                    Width="150px">
+                </asp:DropDownList>
+            </div>
+            <div class="col-3">
+                <span>Route:</span>
+                <asp:DropDownList CssClass="mb-3" ID="ddlRoutes" runat="server" AutoPostBack="True"
+                    Width="150px">
+                </asp:DropDownList>
+            </div>
+            <div class="col-5">
+                <div class="row">
+                    <div class="col-4">
+                        <asp:Button CssClass="btn btn-primary px-3" ID="btnCreateXML" OnClick="btnCreateXML_Click" runat="server" Text="Create XML" />
+                    </div>
+                    <div class="col-4">
+                        <asp:Button CssClass="btn btn-primary px-3" ID="btnImportXML" OnClick="btnImportXML_Click" runat="server" Text="Import" />
+                    </div>
+                    <div class="col-4">
+                        <asp:Button CssClass="btn btn-primary px-3" ID="btnExportXML" OnClick="btnExportXML_Click" runat="server" Text="Export" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-5">
+                <span class="mr-auto">Start time:</span>
+                <asp:TextBox ID="TextBox2" runat="server" Width="200px" type="date"></asp:TextBox>
+            </div>
+            <div class="col-5">
+                <span class="mr-auto">End time:</span>
+                <asp:TextBox ID="tbStartingDate" runat="server" Width="200px" type="date"></asp:TextBox>
+            </div>
+
+        </div>
+        <div class="row mb-2">
+            <div class="col-5">
+                <span class="mr-auto">Start coordinate:</span>
+                <asp:TextBox ID="txtStartCood" runat="server" Width="200px"></asp:TextBox>
+            </div>
+            <div class="col-5">
+                <span class="mr-auto">End coordinate:</span>
+                <asp:TextBox ID="txtEndCood" runat="server" Width="200px"></asp:TextBox>
+            </div>
+        </div>
+        <div class="row d-flex flex-column">
+            <div class="col-5">
+                <span class="mr-auto">Distance(km):</span>
+                <input id="txtDistance" type="number" min="1" max="1500" runat="server" />
+            </div>
+            <div class="col-5 mt-2 mb-2">
+                <span class="mr-auto">Average speed(km/h):</span>
+                <input id="txtAvgspeed" type="number" min="1" max="200" runat="server" />
+            </div>
+            <div class="col-5">
+                <span class="mr-auto">Fuel consumption(litres):</span>
+                <input id="txtFuelConsumption" type="number" min="1" max="100" runat="server" />
+            </div>
+        </div>
+        <asp:Label ID="lblInfo" runat="server" Font-Names="Arial"></asp:Label>
+
+</asp:Content>
---
+++ PPPKProjekt/i3XML.aspx.cs
@@ -0,0 +1,108 @@
+﻿using PPPKProjekt.App_Code;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PPPKProjekt
+{
+    public partial class i3XML : System.Web.UI.Page
+    {
+        string cs = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        List<TravelOrder> orders = new List<TravelOrder>();
+        List<Route> routes = new List<Route>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnImportXML_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+
+            ds.ReadXml(MapPath("App_Data/routes.xml"));
+
+            foreach (DataRow rowDrzava in ds.Tables["TravelOrder"].Rows)
+            {
+                TravelOrder tempOrder = new TravelOrder();
+
+                tempOrder.Id = int.Parse(rowDrzava["Id"].ToString());
+                tempOrder.OrderStatus = int.Parse(rowDrzava["OrderStatus"].ToString());
+                tempOrder.VehicleID = int.Parse(rowDrzava["VehicleID"].ToString());
+                tempOrder.DriverID = int.Parse(rowDrzava["UserID"].ToString());
+                tempOrder.VehicleStartKM = long.Parse(rowDrzava["Vehicle_km_start"].ToString());
+                tempOrder.VehicleEndKM =  long.Parse(rowDrzava["Vehicle_km_end"].ToString());
+                tempOrder.Distance = int.Parse(rowDrzava["Distance_crossed"].ToString());
+                tempOrder.StartingCity = rowDrzava["Starting_city"].ToString();
+                tempOrder.FinishCity = rowDrzava["Finish_city"].ToString();
+                tempOrder.TotalPrice = decimal.Parse(rowDrzava["Total_price"].ToString());
+                tempOrder.TotalDays = int.Parse(rowDrzava["Total_days"].ToString());
+                tempOrder.StartingDate = DateTime.Parse(rowDrzava["StartingDate"].ToString());
+
+                orders.Add(tempOrder);
+                foreach (DataRow rowRoute in ds.Tables["Route"].Rows)
+                {
+                    Route tempRoute = new Route();
+                    tempRoute.Id = int.Parse(rowRoute["Id"].ToString());
+                    tempRoute.TravelOrder = tempOrder;
+                    tempRoute.DateStart = DateTime.Parse(rowRoute["DateStart"].ToString());
+                    tempRoute.DateEnd = DateTime.Parse(rowRoute["DateEnd"].ToString());
+                    tempRoute.StartCoordinate = rowRoute["StartCoordinate"].ToString();
+                    tempRoute.EndCoordinate = rowRoute["EndCoordinate"].ToString();
+                    tempRoute.DistanceCrossed = int.Parse(rowRoute["DistanceCrossed"].ToString());
+                    tempRoute.AvgSpeed = int.Parse(rowRoute["AverageSpeed"].ToString());
+                    tempRoute.FuelConsumption = int.Parse(rowRoute["FuelConsumption"].ToString());
+
+                    routes.Add(tempRoute);
+                }
+            }
+
+            SetField();
+        }
+
+        private void SetField()
+        {
+            ddlTravelOrders.DataSource = orders;
+            ddlTravelOrders.DataBind();
+
+            ddlRoutes.DataSource = routes.Where(x => x.TravelOrder.Id == int.Parse(ddlTravelOrders.SelectedValue));
+            ddlRoutes.DataBind();
+        }
+
+        protected void btnExportXML_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreateXML_Click(object sender, EventArgs e)
+        {
+            CreateXML();
+        }
+
+        private void CreateXML()
+        {
+            SqlConnection con = new SqlConnection(cs);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM TravelOrder; SELECT * FROM Route", con);
+
+            DataSet ds = new DataSet("TravelOrderRoutes");
+            da.Fill(ds);
+
+            ds.Tables[0].TableName = "TravelOrder";
+            ds.Tables[1].TableName = "Route";
+
+            DataRelation relacija = new DataRelation("relacija",
+                ds.Tables[0].Columns["Id"], ds.Tables[1].Columns["TravelOrderID"]);
+            relacija.Nested = true;
+            ds.Relations.Add(relacija);
+
+            ds.WriteXml(MapPath("App_Data/routes.xml"), XmlWriteMode.WriteSchema);
+
+            lblInfo.Text = "XML datoteka spremljena!";
+        }
+    }
+}
